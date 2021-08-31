9fdf76e7a3e4cfd9e402f1477c8a9d23f934a6f5
Nicolás <nicolascaytuirosilva@gmail.com>
2021-08-31 01:17:53 -0500

    Se culmino con la implementación del ejercicio (aun no se muestran las imagenes)

---
+++ AElectrodomesticos/Site.Master.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AElectrodomesticos
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(this.IsPostBack == false)
+            {
+                Electrodomesticos.Items.Add(new ListItem("Cocina", "1"));
+                Electrodomesticos.Items.Add(new ListItem("Licuadora", "2"));
+                Electrodomesticos.Items.Add(new ListItem("Horno Microondas", "3"));
+                Electrodomesticos.Items.Add(new ListItem("Refrigerador", "4"));
+                Electrodomesticos.Items.Add(new ListItem("Televisor", "5"));
+                Electrodomesticos.Items.Add(new ListItem("Aspiradora", "6"));
+                Cocina.Visible = false;
+                Licuadora.Visible = false;
+                Microondas.Visible = false;
+                Refrigerador.Visible = false;
+                Televisor.Visible = false;
+                Aspiradora.Visible = false;
+            }
+        }
+
+        protected void MostrarServerClick(Object sender, EventArgs e)
+        {
+            ListItem seleccion = Electrodomesticos.Items[Electrodomesticos.SelectedIndex];
+            if (seleccion.Value == "1")
+            {
+                Resultado.InnerText = "Usted ha seleccionado la cocina y sus características son:";
+                Cocina.Visible = true;
+                Licuadora.Visible = false;
+                Microondas.Visible = false;
+                Refrigerador.Visible = false;
+                Televisor.Visible = false;
+                Aspiradora.Visible = false;
+            }
+            if (seleccion.Value == "2")
+            {
+                Resultado.InnerText = "Usted ha seleccionado la licuadora";
+                Cocina.Visible = false;
+                Licuadora.Visible = true;
+                Microondas.Visible = false;
+                Refrigerador.Visible = false;
+                Televisor.Visible = false;
+                Aspiradora.Visible = false;
+            }
+            if (seleccion.Value == "3")
+            {
+                Resultado.InnerText = "Usted ha seleccionado el horno microondas";
+                Cocina.Visible = false;
+                Licuadora.Visible = false;
+                Microondas.Visible = true;
+                Refrigerador.Visible = false;
+                Televisor.Visible = false;
+                Aspiradora.Visible = false;
+            }
+            if (seleccion.Value == "4")
+            {
+                Resultado.InnerText = "Usted ha seleccionado el refrigerador";
+                Cocina.Visible = false;
+                Licuadora.Visible = false;
+                Microondas.Visible = false;
+                Refrigerador.Visible = true;
+                Televisor.Visible = false;
+                Aspiradora.Visible = false;
+            }
+            if (seleccion.Value == "5")
+            {
+                Resultado.InnerText = "Usted ha seleccionado el televisor";
+                Cocina.Visible = false;
+                Licuadora.Visible = false;
+                Microondas.Visible = false;
+                Refrigerador.Visible = false;
+                Televisor.Visible = true;
+                Aspiradora.Visible = false;
+            }
+            if (seleccion.Value == "6")
+            {
+                Resultado.InnerText = "Usted ha seleccionado la aspiradora";
+                Cocina.Visible = false;
+                Licuadora.Visible = false;
+                Microondas.Visible = false;
+                Refrigerador.Visible = false;
+                Televisor.Visible = false;
+                Aspiradora.Visible = true;
+            }
+        }
+    }
+}
---
+++ AElectrodomesticos/Site.Master.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace AElectrodomesticos
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Electrodomesticos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect Electrodomesticos;
+
+        /// <summary>
+        /// Control MostrarDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputSubmit MostrarDatos;
+
+        /// <summary>
+        /// Control Resultado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Resultado;
+
+        /// <summary>
+        /// Control Cocina.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Cocina;
+
+        /// <summary>
+        /// Control Licuadora.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Licuadora;
+
+        /// <summary>
+        /// Control Microondas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Microondas;
+
+        /// <summary>
+        /// Control Refrigerador.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Refrigerador;
+
+        /// <summary>
+        /// Control Televisor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Televisor;
+
+        /// <summary>
+        /// Control Aspiradora.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Aspiradora;
+    }
+}
